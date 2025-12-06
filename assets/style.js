d990174919790724e96e67d6bc61ba03cbf8c321
Jacob Emmerson <tae@Jacobs-MacBook-Air.local>
2025-12-06 15:38:24 -0500

    fixed mobile

--- assets/style.js
+++ assets/style.js
@@ -146,18 +146,17 @@ document.querySelector('.profilepic-flip-container').addEventListener('dblclick'
 
 // support double-tap on mobile
 var lastTap;
-document.querySelector('.profilepic-flip-container').addEventListener('touchend', (e) => {
+document.querySelector('.profilepic-flip-container').addEventListener('touchend', function (e) {
     e.preventDefault();
     var now = new Date().getTime();
     var timesince = now - mylatesttap;
     if((timesince < 600) && (timesince > 0)){
-      flipProfile()
+      flipProfile();
     }else{
             // too much time to be a doubletap
           }
 
     lastTap = new Date().getTime();
-
 });
 
 //--------------------------------------------------
